87829ed0a37a6fee6623f1caa01618340ca94313
Bojan Vlatkovic <vlatkovicbojan@gmail.com>
2026-04-25 01:57:13 +0200

    chore: add Jest + ts-jest test infrastructure

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  setupFiles: ['reflect-metadata'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          experimentalDecorators: true,
+          emitDecoratorMetadata: true,
+          strict: true,
+          esModuleInterop: true,
+        },
+      },
+    ],
+  },
+};
