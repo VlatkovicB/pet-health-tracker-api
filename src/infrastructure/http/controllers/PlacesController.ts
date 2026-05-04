fc337b098721f5542746819eb013d1dcf184ad83
Bojan Vlatkovic <vlatkovicbojan@gmail.com>
2026-05-04 18:27:48 +0200

    feat(http): enforce monthly Places search limit

--- src/infrastructure/http/controllers/PlacesController.ts
+++ src/infrastructure/http/controllers/PlacesController.ts
@@ -1,19 +1,24 @@
-import { JsonController, Get, QueryParams, UseBefore } from 'routing-controllers';
+import { JsonController, Get, QueryParams, UseBefore, CurrentUser } from 'routing-controllers';
 import { Service } from 'typedi';
 import { GooglePlacesClient } from '../../external/GooglePlacesClient';
-import { authMiddleware } from '../middleware/authMiddleware';
+import { authMiddleware, AuthPayload } from '../middleware/authMiddleware';
 import { Validate } from '../decorators/Validate';
 import { PlacesSearchQuerySchema, PlacesSearchQuery, PlacesDetailsQuerySchema, PlacesDetailsQuery } from '../schemas/placesSchemas';
+import { LimitService } from '../../../application/limits/LimitService';
 
 @JsonController('/places')
 @Service()
 @UseBefore(authMiddleware)
 export class PlacesController {
-  constructor(private readonly placesClient: GooglePlacesClient) {}
+  constructor(
+    private readonly placesClient: GooglePlacesClient,
+    private readonly limitService: LimitService,
+  ) {}
 
   @Get('/search')
   @Validate({ query: PlacesSearchQuerySchema })
-  async search(@QueryParams() query: PlacesSearchQuery) {
+  async search(@CurrentUser() user: AuthPayload, @QueryParams() query: PlacesSearchQuery) {
+    await this.limitService.checkAndIncrementPlacesSearch(user.userId);
     return this.placesClient.search(query.q);
   }
 
